4b6692626c41da4db14eea7b3448f921fe63cd2e
Adm123 <adm123@mail.ru>
2023-09-08 14:02:34 +0300

    add averageLevel task

---
+++ LevelAverage/src/main/java/ru/adm123/levelAverage/Main.java
@@ -0,0 +1,64 @@
+package ru.adm123.levelAverage;
+
+
+import lombok.NonNull;
+import ru.adm123.levelAverage.structure.Node;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    private static Integer[] root1 = new Integer[]{3, 9, 20, null, null, 15, 7};
+    private static Integer[] root2 = new Integer[]{3, 9, 20, 15, 7};
+
+    public static void main(String[] args) {
+        Node<Integer> tree = Node.from(root2);
+        Float[] levelAverageArray = getLevelAverage(tree);
+        System.out.println(Arrays.toString(levelAverageArray));
+    }
+
+    @NonNull
+    private static Float[] getLevelAverage(@NonNull Node<Integer> node) {
+        Map<Integer, List<Node<Integer>>> levelNodeMap = new HashMap<>();
+        int level = 0;
+        levelNodeMap.put(level, Collections.singletonList(node));
+        if (nodeHasChild(node)) {
+            boolean hasNextLevel = true;
+            while (hasNextLevel) {
+                hasNextLevel = false;
+                for (Node<Integer> nodeInLevel : levelNodeMap.get(level)) {
+                    if (nodeInLevel == null) {
+                        continue;
+                    }
+                    if (nodeHasChild(nodeInLevel)) {
+                        hasNextLevel = true;
+                        levelNodeMap
+                                .computeIfAbsent(level + 1, value -> new ArrayList<>())
+                                .add(nodeInLevel.getLeftChild());
+                        levelNodeMap
+                                .computeIfAbsent(level + 1, value -> new ArrayList<>())
+                                .add(nodeInLevel.getRightChild());
+                    }
+                }
+                level++;
+            }
+        }
+        List<Float> levelAverageList = levelNodeMap.values().stream()
+                .map(Main::getNodeListAverage)
+                .collect(Collectors.toList());
+        return levelAverageList.toArray(levelAverageList.toArray(new Float[0]));
+    }
+
+    private static <T> boolean nodeHasChild(@NonNull Node<T> node) {
+        return node.getLeftChild() != null
+                || node.getRightChild() != null;
+    }
+
+    private static float getNodeListAverage(@NonNull List<Node<Integer>> nodeList) {
+        return nodeList.stream()
+                .mapToInt(Node::getValue)
+                .sum() * 1f / nodeList.size();
+    }
+
+}
--- TreeString/src/main/java/ru/adm123/treeString/Main.java
+++ TreeString/src/main/java/ru/adm123/treeString/Main.java
@@ -3,9 +3,6 @@ package ru.adm123.treeString;
 import lombok.NonNull;
 import ru.adm123.treeString.structure.Node;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Main {
 
     private static Integer[] root0 = new Integer[]{1, 2, 3, null, null, 6, 7};
@@ -13,7 +10,7 @@ public class Main {
     private static Integer[] root2 = new Integer[]{1, 2, 3, null, 4};
 
     public static void main(String[] args) {
-        Node<Integer> tree = createTree(root0);
+        Node<Integer> tree = Node.from(root0);
         String treeString = treeToString(tree);
         System.out.printf("Бинарное дерево из этого массива при обходе его предварительным порядком в виде строки будет %s", treeString);
     }
@@ -38,63 +35,6 @@ public class Main {
                     .append(")");
         }
         return resultCollector.toString();
-    }
-
-    /**
-     * Метод собирает бинарное дерево из массива "значений" узлов
-     *
-     * @param root массив значений узлов
-     * @param <T>  тип значения исходного массива и узла дерева
-     * @return {@link Node} корневой узел дерева
-     */
-    @NonNull
-    private static <T> Node<T> createTree(@NonNull T[] root) {
-        Queue<Node<T>> nodeQueue = arrayToNodeQueue(root);
-        Node<T> rootNode = nodeQueue.poll();
-        if (rootNode == null) {
-            throw new IllegalArgumentException();
-        }
-        int horizontalLevel = 1;
-        List<Node<T>> levelNodeList = new ArrayList<>(Collections.singletonList(rootNode));
-        while (!nodeQueue.isEmpty()) {
-            int levelNodeCount = horizontalLevel * 2;
-            List<Node<T>> nextLevelNodeList = new ArrayList<>(levelNodeCount);
-            for (int i = 0; i < levelNodeCount; i++) {
-                nextLevelNodeList.add(nodeQueue.poll());
-            }
-            for (int i = 0; i < levelNodeList.size(); i++) {
-                Node<T> node = levelNodeList.get(i);
-                if (node != null) {
-                    node.setLeftChild(nextLevelNodeList.get(i * 2));
-                    node.setRightChild(nextLevelNodeList.get(i * 2 + 1));
-                }
-            }
-            levelNodeList = nextLevelNodeList;
-            horizontalLevel++;
-        }
-        return rootNode;
-    }
-
-    /**
-     * Метод преобразует массив в очередь узлов бинарного дерева, типизированных типом элементов массива,
-     * с сохранением порядка следования элементов
-     *
-     * @param array исходный массив
-     * @param <T>   тип данных в массиве
-     * @return {@link Queue<Node>}
-     */
-    @NonNull
-    private static <T> Queue<Node<T>> arrayToNodeQueue(T[] array) {
-        if (array == null) {
-            throw new IllegalArgumentException();
-        }
-        return Arrays.stream(array)
-                .map(Main::nodeFromValue)
-                .collect(Collectors.toCollection(LinkedList::new));
-    }
-
-    private static <T> Node<T> nodeFromValue(T value) {
-        return value == null ? null : new Node<>(value);
     }
 
 }
